e2ad051d50510417d760dd9ff8a3ab7cf3256651
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-05 17:32:11 +0000

    added views.py file

--- Catalog/views.py
+++ Catalog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from .models import Book, Genre, Author
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+def show_books(request):
+    
+    all_books = Book.objects.all().order_by('publishing_year')
+    
+    return render(request, 'Catalog/show_books.template.html', {
+        'all_books':all_books,
+    })
