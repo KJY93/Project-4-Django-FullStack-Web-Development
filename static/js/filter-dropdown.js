64c3ce9e915dcb68c98a2c8f6eaa33c3e9d0887d
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-07 13:55:55 +0000

    added JS code to auto-initialize bootstrap-select using Snapappointments plugin

---
+++ static/js/filter-dropdown.js
@@ -0,0 +1,7 @@
+// Code to auto-initialize bootstrap-select using Snapappointments plugin
+// Credit to Snappointments - https://github.com/snapappointments/bootstrap-select/
+
+document.addEventListener("DOMContentLoaded", function(event) {
+        $("#genreFilter").selectpicker();
+        $("#authorFilter").selectpicker();
+});
