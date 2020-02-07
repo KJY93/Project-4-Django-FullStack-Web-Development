f8bee3b9f26b555d79ac5e0331d2a2c1e27e0ef3
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-07 14:34:39 +0000

    added code to reset the form if the reset button is clicked in

--- static/js/filter-dropdown.js
+++ static/js/filter-dropdown.js
@@ -2,6 +2,22 @@
 // Credit to Snappointments - https://github.com/snapappointments/bootstrap-select/
 
 document.addEventListener("DOMContentLoaded", function(event) {
+        
+        // initialize the bootstrap-select plugin
         $("#genreFilter").selectpicker();
         $("#authorFilter").selectpicker();
+        
+        // if reset button is clicked
+        // remove the selected attribute on option tag 
+        // and reset the option title to Genre and Author respectively
+        $("#resetButton").click(function() {
+            
+            $("#genreFilter option:selected").prop("selected", false);
+            $("#authorFilter option:selected").prop("selected", false);
+            
+            $('#genreFilter').selectpicker({title: 'Genre'}).selectpicker('render');
+            $('#authorFilter').selectpicker({title: 'Author'}).selectpicker('render');
+
+        });
+        
 });
