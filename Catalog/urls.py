52371f8ca36e43ce7b5a7726686a12bd1c72482c
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-08 16:09:16 +0000

    added route for AJAX query to filter books

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from Catalog.views import show_books
+from Catalog.views import show_books, filter_book
 
 urlpatterns = [
-    path('', show_books, name='show_books')
+    path('', show_books, name='show_books'),
+    path('ajax/filter_book/', filter_book, name='filter_book'),
 ]
