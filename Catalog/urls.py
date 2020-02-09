fde1c83e78b4f92ef152282df5beb2641fdf9f69
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-09 08:02:45 +0000

    added urls to handle the book details route

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Catalog.views import show_books, filter_book
+from Catalog.views import show_books, filter_book, book_details
 
 urlpatterns = [
     path('', show_books, name='show_books'),
     path('ajax/filter_book/', filter_book, name='filter_book'),
+    path('book_details/<book_id>', book_details, name='book_details'),
 ]
