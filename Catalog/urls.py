5a4356952774a52e1da78336d5b31ef388718e81
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-11 11:16:33 +0000

    removed filter_books url

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from Catalog.views import show_books, filter_book, book_details
+from Catalog.views import show_books, book_details
 
 urlpatterns = [
     path('', show_books, name='show_books'),
-    path('ajax/filter_book/', filter_book, name='filter_book'),
     path('book_details/<book_id>', book_details, name='book_details'),
 ]
