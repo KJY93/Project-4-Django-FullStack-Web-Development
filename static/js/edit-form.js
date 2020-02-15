01600020523c0b32117318e32645d2bd7edb0b7f
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-15 02:28:39 +0000

    added edit-form.js to update quantity and total price related to a book whenver a user make changes to the quantity of books they would like to order

---
+++ static/js/edit-form.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    // loop through all the books to get each book new quantity
+    for (let i = 0; i < row_of_record; i++) {
+        $(`#qty${i+1}`).on('keyup input', function() {
+            var newqty = $(`#qty${i+1}`).val();
+            var bookId = $(`#book_id${i+1}`).val();
+            var qtyInCart = parseInt($("#qtyInCart").text());
+
+            $.ajax({
+                type: 'get',
+                dataType: 'json',
+                url: 'update/' + bookId,
+                data: {
+                    'newqty': newqty,
+                    'bookId': bookId,
+                    'divId':i+1
+                },
+                success: function(response) {
+                    $(`#totalBookPrice${response[i]['row_id']}`).text("$" + response[i]["total_price"]);
+                    $(`#qtyAvailable${response[i]['row_id']}`).text(response[i]["available_quantity"] + " available");
+                    $("#qtyInCart").text(response[row_of_record]['new_total_quantity']);
+                }
+            });
+            
+            
+        });
+
+    }
+})
