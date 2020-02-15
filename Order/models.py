aa337de7bb6cedebe94be60773bb1b8bef3665f4
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-15 02:45:19 +0000

    create a model for Order in models.py

---
+++ Order/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Charge(models.Model):
+    full_name = models.CharField(max_length=255, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
