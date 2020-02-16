de9ef6eefe98170b590bcd8030b17b5531e5bda8
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-16 16:52:49 +0000

    edit naming of view_profile url

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import login, logout, profile, register
+from .views import login, logout, view_profile, register
 
 urlpatterns = [
     path('', include('Catalog.urls')),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('profile/', profile, name='profile'),
+    path('view_profile/', view_profile, name='view_profile'),
     path('register/', register, name='register'),
 ]
