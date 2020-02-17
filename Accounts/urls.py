e8a2f33cf4edff4315f73c42f0907811c2a424b2
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-17 13:25:22 +0000

    remove unnecessary urls route in urls.py file

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import login, logout, view_profile, register
 
 urlpatterns = [
-    path('', include('Catalog.urls')),
+
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
     path('view_profile/', view_profile, name='view_profile'),
