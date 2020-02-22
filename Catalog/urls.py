0edc716e6f3f3cd6eda5619c6883dd8c79f155ca
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-22 14:44:24 +0000

    modify urls.py file for show_books route and remove book_filter urls route

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-# 160220 add in book filter
-from Catalog.views import show_books, book_details, book_filter
+from .views import show_books, book_details
 
 urlpatterns = [
-    # change from '' to <category> to cater for different category based on user selection
-    path('<category>', show_books, name='show_books'),
+    path('', show_books, name='show_books'),
     path('book_details/<book_id>', book_details, name='book_details'),
-    # add in book filter route
-    path('filter/', book_filter, name='filter'),
 ]
