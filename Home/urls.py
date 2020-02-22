d1f3b7efc1753791ddfa9e65fd73cf54bf6ee458
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-22 14:49:37 +0000

    added urls for Our Services, About Us and Contact Us

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from Home.views import get_index
+# 220220 include our service
+from Home.views import get_index, our_service, about_us, contact_us
 
 urlpatterns = [
     path('', get_index, name='get_index'),
+    path('OurServices/', our_service, name='our_service'),
+    path('AboutUs/', about_us, name='about_us'),
+    path('ContactUs/', contact_us, name='contact_us')
 ]
