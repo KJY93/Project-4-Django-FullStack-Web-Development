c41c6c35afe3d7c87d38d740e073144b6916d652
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-23 11:32:03 +0000

    added view_purchase_list and view_purchase_details routes

--- Order/urls.py
+++ Order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import checkout, confirm_checkout
+from .views import checkout, confirm_checkout, view_purchase_list, view_purchase_details
 
 urlpatterns = [
     path('', checkout, name='checkout'),
-    path('confirm_checkout/', confirm_checkout, name='confirm_checkout'),    
+    path('confirm_checkout/', confirm_checkout, name='confirm_checkout'),   
+    path('view_purchase_list/', view_purchase_list, name='view_purchase_list'),
+    path('view_purchase_details/<order_purchase_id>', view_purchase_details, name='view_purchase_details'),    
 ]
