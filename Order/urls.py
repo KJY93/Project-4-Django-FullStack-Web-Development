79fc98ecb2442e95f41be2cf816318c18e9cbb6a
Ubuntu <ubuntu@ip-172-31-35-87.us-east-2.compute.internal>
2020-02-24 15:40:41 +0000

    added in url to update order route

--- Order/urls.py
+++ Order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import checkout, confirm_checkout, view_purchase_list, view_purchase_details
+from .views import checkout, confirm_checkout, view_purchase_list, view_purchase_details, update_order
 
 urlpatterns = [
     path('', checkout, name='checkout'),
     path('confirm_checkout/', confirm_checkout, name='confirm_checkout'),   
     path('view_purchase_list/', view_purchase_list, name='view_purchase_list'),
     path('view_purchase_details/<order_purchase_id>', view_purchase_details, name='view_purchase_details'),    
+    path('update_order/<order_id>', update_order, name='update_order'),   
 ]
